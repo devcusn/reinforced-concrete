9c60027e4028cdd3350955e42e91e13d66a37ec1
Halil Tunc <hitunc@outlook.com>
2023-10-14 18:46:43 +0300

    feat: slab_thicknes -  edge proportion add

--- Slab.py
+++ Slab.py
@@ -1,17 +1,42 @@
 class Slab():
-    def __init__(self, dimension, type, floor_name):
+    def __init__(self, dimension, edges_type, floor_name, floor_type, config):
         self.x = dimension[0]
         self.y = dimension[1]
-        self.type = type
-        self.name = floor_name
+        self.floor_name = floor_name
+        self.floor_type = floor_type
+        self.edges_type = edges_type
+        self.perimeter = sum(dimension) * 2
+        self.continuous_edge = self.__continues_edge_total()
         self.edge_proportion = self.x/self.y if self.x > self.y else self.y/self.x
         self.how_floor_work = 'single direction' if self.edge_proportion > 2 else 'double direction'
+        self.a_s = self.__continues_edge_proportion()
+        self.config = config
+        self.ls_n = self.__determine_ls_n()
+        self.slab_thicknes = self.__determining_slab_thickness()
 
     def __str__(self):
-        return f"Floor: {self.name}\n" \
+        return f"Floor: {self.floor_name}\n" \
             f"Dimensions: {self.x} x {self.y}\n" \
             f"Edge Proportion: {self.edge_proportion}\n" \
             f"How Floor Works: {self.how_floor_work}"
 
-    def determining_slab_thickness():
-        return 'calculate here'
+    def __determining_slab_thickness(self):
+        # h >0 80m TS(500)
+        if (self.floor_type == 'normal'):
+            return (self.ls_n / (15 + 20 / self.edge_proportion)) * (1-(self.a_s/4))
+        return self.ls_n / 12
+
+    def __continues_edge_proportion(self):
+        return self.continuous_edge / self.perimeter
+
+    def __continues_edge_total(self):
+        return self.edges_type[0] * self.x + self.edges_type[1] * self.y + self.edges_type[2] * self.x + self.edges_type[3] * self.y
+
+    def __determine_ls_n(self):
+        x = self.x
+        y = self.y
+        beam_front_dimension = self.config['beam_front_dimension'] * 10
+        if (self.floor_type == 'balcony'):
+            return y * 1000 - beam_front_dimension / 2 if x > y else x - beam_front_dimension / 2
+
+        return y * 1000 - beam_front_dimension if x > y else x - beam_front_dimension
--- app.py
+++ app.py
@@ -11,7 +11,7 @@ h = 3
 concrete_type = 'C30'
 soil_type = 'ZD'
 q_t = 420
-
+beam_front_dimension = 30  # cm
 # deprem verisi
 [latitude, longitude] = [38.65, 39.65]
 
@@ -30,19 +30,19 @@ print("""
                     |-b2-|
       """)
 
-floors_data = {
-    'd2': {"dimension": [l_x_1, l_y_1], "type": [0, 1, 1, 0]},
-    'd2': {"dimension": [l_x_2, l_y_1], "type": [0, 1, 1, 1]},
-    'd3': {"dimension": [l_x_3, l_y_1], "type": [1, 1, 1, 1]},
-    'd4': {"dimension": [l_x_1, l_y_2], "type": [1, 1, 1, 1]},
-    'd5': {"dimension": [l_x_2, l_y_2], "type": [1, 0, 1, 1]},
-    'd6': {"dimension": [l_x_2, l_y_2], "type": [1, 1, 1, 1]},
-    'b1': {"dimension": [l_x_3, a], "type": [0, 0, 0, 1]},
-    'b2': {"dimension": [a, l_y_2], "type": [1, 0, 0, 0]}
+slabs_data = {
+    'd2': {"dimension": [l_x_1, l_y_1], "edges_type": [0, 1, 1, 0], "slab_type": "normal"},
+    'd2': {"dimension": [l_x_2, l_y_1], "edges_type": [0, 1, 1, 1], "slab_type": "normal"},
+    'd3': {"dimension": [l_x_3, l_y_1], "edges_type": [1, 1, 1, 1], "slab_type": "normal"},
+    'd4': {"dimension": [l_x_1, l_y_2], "edges_type": [1, 1, 1, 1], "slab_type": "normal"},
+    'd5': {"dimension": [l_x_2, l_y_2], "edges_type": [1, 0, 1, 1], "slab_type": "normal"},
+    'd6': {"dimension": [l_x_2, l_y_2], "edges_type": [1, 1, 1, 1], "slab_type": "normal"},
+    'b1': {"dimension": [l_x_3, a], "edges_type": [0, 0, 0, 1], "slab_type": "balcony"},
+    'b2': {"dimension": [a, l_y_2], "edges_type": [1, 0, 0, 0], "slab_type": "balcony"}
 }
 
 
-floors = [Slab(value['dimension'], value['type'], key)
-          for key, value in floors_data.items()]
+floors = [Slab(value['dimension'], value["edges_type"], key, value["slab_type"], {"beam_front_dimension": beam_front_dimension})
+          for key, value in slabs_data.items()]
 
-print(floors[0])
+print(floors[0].slab_thicknes)
