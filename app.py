990a1da11905c9cd17e80b653ba8c41dbd3573af
Halil Tunc <hitunc@outlook.com>
2023-10-15 21:54:38 +0300

    feat: folder organization update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from Slab import *
-from Load import *
-from helper import *
+from src.classes.Slab import *
+from src.classes.Load import *
+from src.helper.helper import *
 
 l_y_1 = 4.2
 l_y_2 = 6.0
