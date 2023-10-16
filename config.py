bf029a4978a96943b59a0d78be4f013a3ef50158
Halil Tunc <hitunc@outlook.com>
2023-10-16 15:53:47 +0300

    feat: config file add

---
+++ config.py
@@ -0,0 +1,15 @@
+# project data
+l_y_1 = 4.2
+l_y_2 = 6.0
+l_x_1 = 4.6
+l_x_2 = 4.6
+l_x_3 = 5.8
+a = 1.2
+h = 3
+
+concrete_type = 'C30'
+soil_type = 'ZD'
+q_t = 420
+beam_front_dimension = 30  # cm
+# for earthquake data
+[latitude, longitude] = [38.65, 39.65]
